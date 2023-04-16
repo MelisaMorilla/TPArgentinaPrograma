72102f97279e6a4efeaa0b237f20064186a118bb
MelisaMorilla <129861711+MelisaMorilla@users.noreply.github.com>
2023-04-16 20:26:47 -0300

    Add files via upload

--- TPapp.js
+++ TPapp.js
@@ -1,24 +1,6 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+// boton enviar
 
-  const nextBtn = document.querySelector("#next-btn");
+const nextBtn = document.querySelector("#next-btn");
 const backBtn = document.querySelector("#back-btn");
 const step1 = document.querySelector("#step-1");
 const step2 = document.querySelector("#step-2");
